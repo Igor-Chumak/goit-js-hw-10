a345437bbd234ace79a04b9faa152421620261b3
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-06-08 09:24:15 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,4 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // npm install @slim-select/vue
 import { defineComponent } from 'vue';
 import SlimSelect from '@slim-select/vue';
+import 'slim-select/dist/slimselect.css';
