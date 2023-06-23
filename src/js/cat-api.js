3a7cb894b5c61c215b174471f276178accbfc1e8
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-06-23 23:08:18 +0300

    tmp

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,3 +1,12 @@
 // https://thecatapi.com/ email: chigorv@gmail.com user: chigorvik
-const KEY =
+// request api https://api.thecatapi.com/v1/images/search?
+const API_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
   'api_key=live_yW6ggR3i34HsiLDgf08CYeP40RDSpnwnrV2v1Rdhq6jx8BHcc17ytNdQVYHj12Vz';
+
+function fetchBreeds() {
+  const url = `${API_URL}/breeds?${API_KEY}`;
+  return fetch(url);
+}
+
+export { fetchBreeds };
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,20 @@
+import '../css/style.css';
+
 // npm i notiflix
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 // npm install @slim-select/vue
-import { defineComponent } from 'vue';
-import SlimSelect from '@slim-select/vue';
-import 'slim-select/dist/slimselect.css';
+// import { defineComponent } from 'vue';
+// import SlimSelect from '@slim-select/vue';
+// import 'slim-select/dist/slimselect.css';
+
+const refs = {
+  select: document.querySelector('.breed-select'),
+  animalCard: document.querySelector('.cat-info'),
+  loaderWait: document.querySelector('.loader'),
+  alertError: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
+};
+
+refs.loaderWait.classList.add('is-hidden');
+refs.alertError.classList.add('is-hidden');
